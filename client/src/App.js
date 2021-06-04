e449f2fe8af9954db839918b6c89aab8a8f2411c
Matthias Wahr <matthias.wahr@gmx.de>
2021-06-04 14:44:20 +0200

    inital server.js and fetch localhost

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [serverMessage, setServerMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:4000/')
+      .then((res) => res.json())
+      .then((response) => setServerMessage(response));
+  });
+
   return (
     <div className="App">
-      <h1>Get Vaccinated</h1>
+      <h1>{serverMessage}</h1>
     </div>
   );
 }
