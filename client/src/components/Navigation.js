30d83f14241306ed5d228e13365f051d81256cda
Matthias Wahr <matthias.wahr@gmx.de>
2021-06-16 23:52:40 +0200

    feature-branch

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-export default function Navigation() {
-  return <></>;
-}
-=======
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 export default function Navigation() {
@@ -18,4 +13,3 @@ export default function Navigation() {
 const NavigationBar = styled.nav`
   display: flex;
 `;
->>>>>>> 19e7a1b0c7f0f0a65eb65c919b0a2c4d417a1c2c
