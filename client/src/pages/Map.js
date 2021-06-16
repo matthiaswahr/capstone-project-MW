ed38e22989e9e540f6245b096960e9b0a96d083f
Matthias Wahr <matthias.wahr@gmx.de>
2021-06-16 12:10:41 +0200

    added Map with leaflet

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,5 +1,34 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import styled from 'styled-components';
 
 export default function Map() {
-  return <h1>Hier geht es zum nächsten Impfzentrum</h1>;
+  return (
+    <>
+      <h1>Hier geht es zum nächsten Impfzentrum</h1>
+      <MapWrapper id="mapid">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </MapWrapper>
+    </>
+  );
 }
+
+const MapWrapper = styled.div`
+  .leaflet-container {
+    height: 11.25rem;
+    width: 600px;
+  }
+`;
